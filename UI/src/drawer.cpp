a7735a18250b4ab886c7269e6a5f053dd9d649ea
Elizaveta <e.gryumova@yandex.ru>
2021-12-06 17:06:24 +0300

    update UI

---
+++ UI/src/drawer.cpp
@@ -0,0 +1,50 @@
+#include "drawer.h"
+
+Drawer::Drawer() {
+    view_pos.x = 0;
+    view_pos.y = 0;
+    floor = 0;
+    QPen pen;
+}
+
+Drawer::~Drawer() {
+ 
+}
+
+QPoint Drawer::rePoint(Point &point) {
+    return QPoint(point.x*scale + view_pos.x, point.y*scale + view_pos.y);
+}
+
+void Drawer::setFloor(int new_floor) {
+    this->floor = new_floor;
+    qDebug() << "Floor changed";
+}
+
+void Drawer::setScale(qreal new_scale) {
+    this->scale = new_scale;
+    qDebug() << "Scale changed";
+}
+
+void Drawer::drawLine(QPainter *painter, Point start, Point end) {
+    painter->setPen(pen);
+    painter->drawLine(rePoint(start), rePoint(end));
+}
+
+void Drawer::setColor(Color &color) {
+    pen.setColor(QColor(color.r, color.g, color.b));
+}
+
+void Drawer::setViewPos(Point new_pos) {
+    view_pos.x = new_pos.x;
+    view_pos.y = new_pos.y;
+}
+
+
+qreal Drawer::getScale() {
+    return scale;
+}
+
+
+Point Drawer::getViewPos() {
+    return view_pos;
+}
